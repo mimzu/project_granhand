6067e644cb1a2f2a11f8b5c21edc4563fe7bab91
passminju <passminju@gmail.com>
2025-11-25 10:54:15 +0900

    헤더 색 변경 해결 필요

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,32 @@
 
 
-/* 스크롤 시 헤더 색 변경 */
-document.addEventListener ("scroll",function() {
-    const header = document.querySelector ("header");
-    const mainBnr = document.querySelector (".main_brn_swiper");
-    const logoImg = header.querySelector ('.logo img')
-    const icons = header.querySelectorAll ('.right_icons img')
+document.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    const mainBnr = document.querySelector(".main_brn_swiper");
+    const logoImg = header.querySelector('.logo img');
+    const icons = header.querySelectorAll('.right_icons img');
+    const menuLinks = header.querySelectorAll('.menu a'); // ★ 메뉴 글자들
 
     const bannerHeight = mainBnr.offsetHeight;
     const scrollTop = window.scrollY;
 
     if (scrollTop > bannerHeight - 50) {
-        header.classList.add ("scroll");
+        header.classList.add("scroll");
+
         logoImg.src = './images/logo_scroll.png';
-        icons[0].src ='./images/search_scroll.png';
-        icons[1].src ='./images/profile_scroll.png';
-        icons[2].src ='./images/bag_scroll.png';
+        icons[0].src = './images/search_scroll.png';
+        icons[1].src = './images/profile_scroll.png';
+        icons[2].src = './images/bag_scroll.png';
+
     } else {
-        header.classList.remove ("scroll")
+        header.classList.remove("scroll");
+
         logoImg.src = './images/logo.png';
-        icons[0].src ='./images/search_icon.png';
-        icons[1].src ='./images/profile_icon.png';
-        icons[2].src ='./images/bag_icon.png';
+        icons[0].src = './images/search_icon.png';
+        icons[1].src = './images/profile_icon.png';
+        icons[2].src = './images/bag_icon.png';
     }
-})
+});
 
 // 모든 a 태그 클릭 시 기본 동작 막기
 const allLinks = document.querySelectorAll('a');
